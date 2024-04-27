4646e4c404ec3b48a03cfe15fbebd5d05b63e743
Rareș Gabriel Băeșu <baesu_r@yahoo.com>
2024-04-27 13:51:22 +0300

    [ISSUE-4] Storybook setup

---
+++ src/app/components/t-grid/t-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 't-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './t-grid.component.html',
+  styleUrl: './t-grid.component.scss'
+})
+export class TGridComponent {
+
+}
---
+++ src/stories/t-grid/t-grid.stories.ts
@@ -0,0 +1,19 @@
+import { StoryFn, type Meta } from '@storybook/angular';
+import { TGridComponent } from '../../app/components/t-grid/t-grid.component';
+
+const meta: Meta<TGridComponent> = {
+  title: 'Assignment/t-grid',
+  component: TGridComponent,
+  tags: ['autodocs'],
+  
+};
+
+export default meta;
+
+export const Basic: StoryFn<TGridComponent> = (args: TGridComponent) => ({
+    props: args,
+    template: `
+        <t-grid></t-grid>
+    `
+})
+Basic.args = {}
