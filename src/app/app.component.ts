9aedf3cc6edc0965b08eb1a0d5365bc7c622d39c
Rareș Gabriel Băeșu <baesu_r@yahoo.com>
2024-04-30 04:12:24 +0300

    [ISSUE-40] Updated app component to contain some basic examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TGridComponent } from './components/t-grid/t-grid.component';
+import { TProgressComponent } from './components/t-progress/t-progress.component';
+import { MockUser, createMockData } from '../stories/t-grid/utils';
+import { TColumnComponent } from './components/t-column/t-column.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TGridComponent, TColumnComponent, TProgressComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'home-assignment';
+  data: MockUser[] = createMockData(100);
 }
